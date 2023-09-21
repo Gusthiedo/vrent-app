045a094f2b24c8e3636bc193032f0ccc1c945bba
Hadis S <itshadis2nd@gmail.com>
2023-09-21 14:39:45 +0700

    upadet

--- frontend/src/feautres/components/book/PickDate.jsx
+++ frontend/src/feautres/components/book/PickDate.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable react/prop-types */
 import calendar from '../../../assets/icons/icon-calendar.png';
 
-function PickDate({ children }) {
+function PickDate({ children, setDate, contoh }) {
   return (
     <div className='w-full flex flex-col gap-1'>
       <label htmlFor="select-pick-date" className="font-medium flex items-center gap-1 mb-1">
         <img src={calendar} alt="" />
         { children }
       </label>
-      <input type="date" name='select-pick-date' className='text-grey-text border border-slate-400 h-10 rounded indent-2 focus:outline-none' required />
+      <input value={contoh} onChange={(e) => setDate(e.target.value)} type="date" name='select-pick-date' className='text-grey-text border border-slate-400 h-10 rounded indent-2 focus:outline-none' required />
     </div>
   )
 }
--- frontend/src/feautres/components/book/SelectCar.jsx
+++ frontend/src/feautres/components/book/SelectCar.jsx
@@ -2,9 +2,10 @@ import car from '../../../assets/icons/icon-car.png';
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const SelectCar = () => {
+
+// eslint-disable-next-line react/prop-types
+const SelectCar = ({ setCar, setCarId }) => {
   const [Cars, setCars] = useState();
-  const [selectedCar, setSelectedCar] = useState();
   
   useEffect(() => {
     getCars();
@@ -14,6 +15,12 @@ const SelectCar = () => {
     const response = await axios.get("http://localhost:3003/api/cars");
     setCars(response.data.data);
   };
+
+  const handleOnChange = (e) => {
+    e.preventDefault();
+    setCarId(e.target.selectedIndex);
+    setCar(e.target.value);
+  }
 
   return (
     <div className="flex flex-col gap-1">
@@ -27,14 +34,13 @@ const SelectCar = () => {
         className="text-grey-text border border-slate-400 h-10 rounded indent-2 focus:outline-none"
         name="select-car"
         required
-        value={selectedCar}
-        onChange={(e) => setSelectedCar(e.target.value)}
+        onChange={handleOnChange}
         defaultValue={"Select your car.."}
       >
         <option value="" disabled>Select your car..</option>
         {
           Cars && Cars.map((car, index) => (
-            <option key={index}>{car.name}</option>
+            <option id={car.id_car} key={index}>{car.name}</option>
           ))
         }
       </select>
